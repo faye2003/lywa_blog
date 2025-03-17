ecd648162d7b032a081b8229376295c96741e67c
faye2003 <fayem7409@gmail.com>
2025-03-17 15:32:02 +0100

    update

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Auteur, Article
+
+admin.site.register(Auteur)
+admin.site.register(Article)
+
 # Register your models here.
--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 from django_countries.fields import CountryField
+from django.utils import timezone
 
 class Auteur(models.Model):
     nom_complet = models.CharField(max_length=100)
     date_naissance = models.DateField("Date de naissance")
     pays = CountryField()
+
+    def __str__(self):
+        return self.nom_complet
+
+    @property
+    def age(self):
+        import datetime
+        age = datetime.date() - self.date_naissance
+        return age
 
 # Création du model Article
 class Article(models.Model):
@@ -13,3 +23,6 @@ class Article(models.Model):
     date_publication = models.DateTimeField("Date de publication")
     auteur_id = models.ForeignKey(Auteur, on_delete=models.CASCADE)
     image = models.ImageField("Image")
+
+    def __str__(self):
+        return self.titre
