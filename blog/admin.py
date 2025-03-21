41574ff06d7be8aec6ee274cbf4c7034d0f03622
faye2003 <fayem7409@gmail.com>
2025-03-21 06:53:15 +0100

    update

--- blog/admin.py
+++ blog/admin.py
@@ -4,7 +4,9 @@ from .models import Article
 
 # Lien site pour personaliser la page d'administration
 # https://dev.to/vijaysoni007/how-to-show-images-of-the-model-in-django-admin-5hk4
+class ItemArticle(admin.ModelAdmin):
+    list_display = ['titre', 'auteur', 'date_publication']
 
-admin.site.register(Article)
+admin.site.register(Article, ItemArticle)
 
 # Register your models here.
